d63bda4e40180caf2480fed57f7070d7b9c8b38d
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-18 12:14:54 +0500

    Upper case runnable added

--- Basics/day6.py
+++ Basics/day6.py
@@ -5,3 +5,8 @@ def square(numbers):
 
 square_runnable = RunnableLambda(square)
 print(square_runnable.invoke(10))
+
+def uppercase(text:str):
+    return text.upper()
+uppercase_runnable = RunnableLambda(uppercase)
+print(uppercase_runnable.invoke("Python"))
