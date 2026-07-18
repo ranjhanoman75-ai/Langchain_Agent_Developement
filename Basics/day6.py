40ecf70662b1fa4ba29eac648cca56008afffb7e
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-18 12:24:35 +0500

    Using model added runnable lamda

--- Basics/day6.py
+++ Basics/day6.py
@@ -1,20 +1,18 @@
 print("==========Day 6=======RunnableLamda=============")
 from langchain_core.runnables import RunnableLambda
-def square(numbers):
-    return numbers * numbers
-
-square_runnable = RunnableLambda(square)
-print(square_runnable.invoke(10))
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.output_parsers import StrOutputParser
+load_dotenv()
+model = ChatGroq(model = "llama-3.3-70b-versatile")
+parser = StrOutputParser()
 
 def uppercase(text:str):
     return text.upper()
 uppercase_runnable = RunnableLambda(uppercase)
-response  = uppercase_runnable.batch(
-    [
-        "noman",
-        "mukurram",
-        "bilal"
-    ]
-)
+chain = uppercase_runnable | model | parser
+response  = uppercase_runnable.invoke(
+    "Explain the python loops"
+)   
 print(response)
-print(uppercase_runnable.invoke("Python"))
+
