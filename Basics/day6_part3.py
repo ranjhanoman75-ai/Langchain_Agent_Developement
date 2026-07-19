e557bbd7137bfef257ac23f5e3038d0b713fe170
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-19 10:33:18 +0500

    Runnable parallel added in gen ai with three prompts

--- Basics/day6_part3.py
+++ Basics/day6_part3.py
@@ -1,18 +1,54 @@
 from langchain_groq import ChatGroq
-from langchain_core.runnables import RunnableParallel,RunnablePassthrough,RunnableLambda
+from langchain_core.runnables import RunnableParallel
+from dotenv import load_dotenv 
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+load_dotenv()
+model = ChatGroq(model = "llama-3.3-70b-versatile")
 
-def uppercase(text:str):
-    return text.upper()
-def length(text:str):
-    return len(text)
-length_runnable = RunnableLambda(length)
-uppercase_runnable = RunnableLambda(uppercase)
-parallel = RunnableParallel(
-    original = RunnablePassthrough(),
-    uppercase = uppercase_runnable,
-    length  = length_runnable
+parser = StrOutputParser()
+summary_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are professional python teacher."),
+        ("human", "give a short summary of {topic}.")
+    ]
 )
-response = parallel.invoke(
-    "python"
+code_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system","You are an expert python developer."),
+        ("human", "Give the python code example for {topic}.")
+    ]
 )
-print(response)
+interviewer_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are python Interviewer."),
+        ("human", "Generate 5 interview question about {topic}")
+    ]
+)
+summary_chain = summary_prompt | model | parser
+code_chain = code_prompt | model | parser
+interview_chain = interviewer_prompt | model | parser
+
+parallel_chain= RunnableParallel(
+    summary = summary_chain,
+    code = code_chain,
+    Interview_question = interview_chain
+)
+response = parallel_chain.invoke(
+    {
+         "topic":"python decorators"
+    }
+   
+)
+print("="*60)
+print("Summary")
+print("="*60)
+print(response["summary"])
+print("="*60)
+print("Code")
+print("="*60)
+print(response["code"])
+print("="*60)
+print("Interview Question")
+print("="*60)
+print(response["Interview_question"])
